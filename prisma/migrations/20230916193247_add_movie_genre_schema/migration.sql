22f2125e75cb832529dfd2d133bf5352fc705a20
Sennay <wsennay@gmail.com>
2023-09-16 22:34:02 +0300

    add movie genre schema

---
+++ prisma/migrations/20230916193247_add_movie_genre_schema/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Genre" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MovieGenre" (
+    "movieId" TEXT NOT NULL,
+    "genreId" TEXT NOT NULL,
+
+    CONSTRAINT "MovieGenre_pkey" PRIMARY KEY ("movieId","genreId")
+);
+
+-- AddForeignKey
+ALTER TABLE "MovieGenre" ADD CONSTRAINT "MovieGenre_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MovieGenre" ADD CONSTRAINT "MovieGenre_genreId_fkey" FOREIGN KEY ("genreId") REFERENCES "Genre"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
