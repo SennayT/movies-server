a2c0650b92975b2d68409b977565820d889fc9b7
Sennay <wsennay@gmail.com>
2023-09-16 21:58:42 +0300

    read port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { AppConfigService } from './app-config/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = app.get(AppConfigService);
+  const PORT = config.get('PORT');
+
+  const logger = new Logger('bootstrap');
+
+  await app.listen(PORT);
+  logger.debug(`Server running on port ${PORT}`);
 }
 bootstrap();
